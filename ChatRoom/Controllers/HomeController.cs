eeb409cdd728b96d4ad88e0f0dab54664192dbd7
benny <zhaopeiym@163.com>
2017-08-12 21:41:45 +0800

    聊天室

---
+++ ChatRoom/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ChatRoom.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.HistoricalMessg = JsonConvert.SerializeObject(SocketHandler.historicalMessg);
+            return View();          
+        }
+     
+    }
+}
